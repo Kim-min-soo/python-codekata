1bf34938fd70449229dca8b1b0e560b3ea92841f
Kim-min-soo <msoo5880@yu.ac.kr>
2026-01-19 08:57:24 +0900

    [프로그래머스] L0 - 짝수의 합 (#11)

---
+++ programmers/L0_기초입문/입문/011_짝수의_합.py
@@ -0,0 +1,13 @@
+# 짝수의 합
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120831
+# 알고리즘: 기초
+# 작성자: 김민수
+# 작성일: 2026. 01. 19. 08:57:24
+
+def solution(n):
+    even_sum = 0
+    for i in range(n+1):
+        if i % 2 == 0 :
+            even_sum += i
+    return even_sum
