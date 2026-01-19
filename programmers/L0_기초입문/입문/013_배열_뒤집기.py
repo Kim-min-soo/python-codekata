b1ea5c9b5400d169bd5e53f7c828932bcd2e1c2b
Kim-min-soo <msoo5880@yu.ac.kr>
2026-01-19 09:03:24 +0900

    [프로그래머스] L0 - 배열 뒤집기 (#13)

---
+++ programmers/L0_기초입문/입문/013_배열_뒤집기.py
@@ -0,0 +1,10 @@
+# 배열 뒤집기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120821
+# 알고리즘: 기초
+# 작성자: 김민수
+# 작성일: 2026. 01. 19. 09:03:24
+
+def solution(num_list):
+    reverse_num_list = num_list[::-1]
+    return reverse_num_list
